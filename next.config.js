2c234ab95fe981ca7a17c8949cc0d3af16bc2f55
Ken Rimple <krimple@chariotsolutions.com>
2022-03-21 17:38:22 -0400

    initial commit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  experimental: {
+    runtime: 'edge',
+    reactRoot: 'concurrent',
+    serverComponents: true
+  }
 }
 
 module.exports = nextConfig
